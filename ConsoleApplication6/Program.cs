be11d6491a29e3efd61fa24860967089a2ed509f
mujeeb <saheilsidd@gmail.com>
2017-11-09 16:28:40 +0100

    Commiting

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -83,7 +83,7 @@ namespace Test
             Console.WriteLine("Microsoft Visual C-- 2015: " + IsSoftwareInstalled("Microsoft Visual C-- 2015", null));
             Console.WriteLine("Microsoft Visual C++ 2015: " + IsSoftwareInstalled("Microsoft Visual C++ 2015", null));
             Console.WriteLine("Microsoft Visual C++ 2015 Redistributable (x86): " + IsSoftwareInstalled("Microsoft Visual C++ 2015", ProgramVersion.x86));
-            Console.WriteLine("Microsoft Visual C++ 2015 Redistributable (x64): " + IsSoftwareInstalled("Microsoft Visual C++ 2015", ProgramVersion.x64));
+            Console.WriteLine("Microsoft Visual888 C++ 2015 Redistributable (x64): " + IsSoftwareInstalled("Microsoft Visual C++ 2015", ProgramVersion.x64));
             Console.ReadKey();
             
             
